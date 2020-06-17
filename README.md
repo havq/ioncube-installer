fad003db670709b3e9c37b854ed99031337e0655
havq <havq.91@gmail.com>
2020-06-17 16:47:12 +0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ioncube-installer
+# Ioncube loader installer
+
+## Install ioncube loader for php5.6 - php7.3 on Ubuntu
+
+* Tested on Ubuntu 16.04 LTS & 18.04 LTS
+
+### Usage
+
+```bash
+bash <(wget -qO - hoangha.net/ioncube) <PHP VERSION>
+```
+
+Example for php7.3: 
+
+```bash
+bash <(wget -qO - hoangha.net/ioncube) 7.3
+```
+
+If php version is not set as argument, the script will automatically install ioncube for the newest php version installed
---
+++ ioncube.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# check if a command exist
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
+if ! command_exists curl; then
+    "curl isn't installed and is required"
+    exit 1
+fi
+
+cd /tmp || exit 1
+rm -rf /tmp/ioncube
+
+if [ -n "$1" ]; then
+    if [ -x "/usr/bin/php$1" ]; then
+        PHP_VER="$1"
+    else
+        echo "php$1 isn't installed"
+        exit 1
+    fi
+else
+    if [ -x "/usr/bin/php" ]; then
+        PHP_VER=$(readlink -f /etc/alternatives/php | awk -F "/usr/bin/php" '{print $2}')
+    fi
+fi
+
+if [ "$PHP_VER" = "7.4" ]; then
+    echo "PHP 7.4 is not supported by ioncube loader yet"
+    exit 1
+fi
+
+EXTENSION_DIR=$(/usr/bin/php${PHP_VER} -i | grep extension_dir | awk -F "=> " '{print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+curl -sSL https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz | tar -xzf - -C /tmp
+cd /tmp/ioncube || exit 1
+cp ioncube_loader_lin_${PHP_VER}.so "${EXTENSION_DIR}/" -f
+
+if [ ! -f "${EXTENSION_DIR}/ioncube_loader_lin_${PHP_VER}.so" ]; then
+    exit 1
+fi
+
+if ! grep -q "ioncube" -r /etc/php/${PHP_VER}/mods-available; then
+    echo -e "; configuration for php ioncube loader\n; priority=00\nzend_extension=ioncube_loader_lin_${PHP_VER}.so" >/etc/php/${PHP_VER}/mods-available/ioncube-loader.ini
+fi
+if ! grep -q "ioncube" -R /etc/php/${PHP_VER}/fpm/conf.d; then
+    phpenmod -v "$PHP_VER" ioncube-loader
+fi
+if ! grep -q "ioncube" -R /etc/php/${PHP_VER}/cli/conf.d; then
+    phpenmod -v "$PHP_VER" ioncube-loader
+fi
+
+service php${PHP_VER}-fpm restart
+cd || exit 1
+rm -rf /tmp/ioncube
